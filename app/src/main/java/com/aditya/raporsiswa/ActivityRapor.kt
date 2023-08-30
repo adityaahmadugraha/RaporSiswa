4f10a44c40f95d96d53701fe29bb5d677a25b4e6
adity <adityaahmadnugraha091@gmail.com>
2023-08-30 13:50:39 +0700

    put data edit

--- app/src/main/java/com/aditya/raporsiswa/ActivityRapor.kt
+++ app/src/main/java/com/aditya/raporsiswa/ActivityRapor.kt
@@ -3,7 +3,6 @@ package com.aditya.raporsiswa
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import com.aditya.raporsiswa.databinding.ActivityRaporBinding
 
 class ActivityRapor : AppCompatActivity() {
